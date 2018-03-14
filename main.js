91650248dd347bf4c42b6ec0bca3588cf5006a57
Shoblik <shoblik@yahoo.com>
2018-03-13 20:44:09 -0700

    I have tiles flipping on hover

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   initializeApp();
+});
+
+function initializeApp() {
+   $('.flip-container').on('mouseover', function() {
+   });
+}
+
+function createTile(tileCount) {
+    for (let i = 0; i < tileCount; i++) {
+
+    }
+}
