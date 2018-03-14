d655dd1b670c2aaeed1a9ca846a36caae5dadfe9
Shoblik <shoblik@yahoo.com>
2018-03-13 21:41:03 -0700

    tile flipping top row left to right

--- main.js
+++ main.js
@@ -3,12 +3,32 @@ $(document).ready(function() {
 });
 
 function initializeApp() {
-   $('.flip-container').on('mouseover', function() {
-   });
+   leftToRightFlip();
 }
 
-function createTile(tileCount) {
-    for (let i = 0; i < tileCount; i++) {
+function leftToRightFlip() {
+    let screenWidth = window.innerWidth;
+    let tileWidth = $('.flip-container').css('width');
+    for (let i = 0; i < tileWidth.length; i++) {
+        if (tileWidth[i] === 'p') {
+            tileWidth = tileWidth.slice(0, i);
+        }
+    }
+    let multiplier = Math.ceil(screenWidth / tileWidth);
 
+    //column iterator
+    let row = 0;
+    let timer = 0;
+    for (let column = 0; column < multiplier; column++) {
+        setTimeout(function() {
+            $($('.flipper')[row * 40 + column]).css('transform', 'rotateY(180deg)');
+        }, timer);
+        timer += 50;
     }
 }
+
+// function createTile(tileCount) {
+//     for (let i = 0; i < tileCount; i++) {
+//
+//     }
+// }
